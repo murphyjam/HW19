6fea294cd878bb25a58215f490c77034ce55a04a
murphyjam <127764989+murphyjam@users.noreply.github.com>
2023-08-25 14:34:08 -0400

    final

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("your index.js is loaded correctly");
+$(document).ready(function() 
+{ 
+    $("#focusb").click(function() 
+    {
+         $("#flexContainer").slideDown("slow");
+})
+$("#workb").click(function() 
+{
+     $(".workgrid").slideDown("slow");
+})
+}
+)
